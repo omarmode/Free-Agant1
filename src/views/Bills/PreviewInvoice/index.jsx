cfb78c6cb442dd01254b5d667a3581a0e5abc77b
omarmode <https://github.com/omarmode>
2024-10-15 18:59:58 +0300

    first commit

---
+++ src/views/Bills/PreviewInvoice/index.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import classNames from "classnames";
+import InvoiceAppSidebar from "../InvoiceAppSidebar";
+import Body from "./Body";
+import Header from "./Header";
+
+const PreviewBills = () => {
+  const [showSidebar, setShowSidebar] = useState(true);
+
+  return (
+    <div className="hk-pg-body py-0">
+      <div
+        className={classNames("invoiceapp-wrap", {
+          "invoiceapp-sidebar-toggle": !showSidebar,
+        })}
+      >
+        <InvoiceAppSidebar />
+        <div className="invoiceapp-content">
+          <div className="invoiceapp-detail-wrap">
+            <Header
+              toggleSidebar={() => setShowSidebar(!showSidebar)}
+              show={showSidebar}
+            />
+            <Body />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PreviewBills;
